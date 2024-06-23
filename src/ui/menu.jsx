42cdc6a10c1383ce0a86f3b2899a1b9f98bb0e69
dvlpr2003 <gayathrigaya698@gmail.com>
2024-06-23 11:04:18 +0530

    dev

--- src/ui/menu.jsx
+++ src/ui/menu.jsx
@@ -28,9 +28,10 @@ const MenuItems = ({Menudata})=>{
  
     return(
         
-        <main   className = "flex flex-column center scroll main-padding">
+        <main   className = "flex flex-column center scroll width-100 pad-t-b">
 
-            <div className="flex flex-column center width-100 border">
+            <div className="flex flex-column center width-100 gap  ">
+
             {
             Menudata.map((e)=><Items key={e.id} name={e.name} price ={e.unitPrice} imageurl ={e.imageUrl} soldOut={e.soldOut} ingredients={e.ingredients}/>)
             }
@@ -43,19 +44,21 @@ const MenuItems = ({Menudata})=>{
 const Items =({name,price,imageurl,soldOut,ingredients})=>{
 
     return(
-        <li className="flex  pizza-item  width-100 border">
+       
+        
+        <div className="flex  pizza-item">
             <img src={imageurl} alt="" onClick={()=>alert("hi")} className="pizza-img"/>
-            <div className="flex flex-column justify-content-center">
+            <div className="flex flex-column justify-content-center width-100">
                 <span>{name}</span>
-                <div className="flex ingredients ">
+                <div className="flex ingredients felx-wrap ">
                 {
                 ingredients.map((e)=><span>{e}</span>)
                 }
                 </div>
-                <div className="flex item-center ">
+                <div className="flex item-center flex-wrap">
                     <span>€{price}.00</span>
-                    <div className="flex item-center gap">
-                        <div className="flex item-center gap-1rem">
+                    <div className="flex item-center gap price-options margin-left-auto ">
+                        <div className="flex item-center gap-1rem ">
                             <button className="inc-dec-btn">-</button>
                             <span>1</span>
                             <button className="inc-dec-btn">+</button>
@@ -64,7 +67,8 @@ const Items =({name,price,imageurl,soldOut,ingredients})=>{
                     </div>
                 </div>
             </div>
-        </li>
+        </div>
+        
     )
 }
 export const CartNav = ()=>{
